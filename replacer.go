eb1f812dea8ea816dc9d31df0308d0176e6c7579
Marcel Schramm <marceloschr@googlemail.com>
2019-02-15 12:14:11 +0100

    Add an example to replacer docs

--- replacer.go
+++ replacer.go
@@ -5,7 +5,12 @@ import "regexp"
 var emojiCodeRegex = regexp.MustCompile("(?s):[a-zA-Z0-9_]+:")
 
 // Replace all emoji sequences contained in the discord emoji map with their
-// respective emojis
+// respective emojis.
+//
+// Examples for valid input:
+//     Replace("Hello World :sun_with_face:")
+// would result in
+//     "Hello World 🌞"
 func Replace(input string) string {
 	if len(input) <= 2 {
 		return input
