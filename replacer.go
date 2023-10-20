d4629182a0160106a78c2a73e24da74f37c3afe8
Marcel Schramm <marceloschr@googlemail.com>
2023-10-20 16:15:36 +0200

    Optimise replace further

--- replacer.go
+++ replacer.go
@@ -17,9 +17,6 @@ func Replace(input string) string {
 		return input
 	}
 
-	// In order to avoid wasting memory, we don't use bytes.buffer, but do it ourselves.
-	var buffer []byte
-
 	start := -1
 	var lastEnd int
 	// Instead of for ranging over the string, we treat it as a byte array to
@@ -33,9 +30,6 @@ func Replace(input string) string {
 			continue
 		}
 
-		// Start of new sequence or an occurence of something like "::+1:" in
-		// which case we want to ignore the first colon and start again from
-		// the next one, as it might be a valid sequence.
 		if start == -1 || index-start == 1 {
 			start = index
 			continue
@@ -55,20 +49,52 @@ func Replace(input string) string {
 			continue
 		}
 
-		if len(buffer) == 0 {
+		var buffer []byte
+		if len(input) <= 32 {
+			buffer = make([]byte, 0, 32)
+		} else {
 			// If we have more than one emoji sequence, we might end up with a
 			// buffer bigger than necessary, but that's fine.
 			buffer = make([]byte, 0, len(input)-len(emojiSequence)-2+len(emojified))
 		}
+
 		buffer = append(buffer, input[lastEnd:start]...)
 		buffer = append(buffer, emojified...)
-		start = -1
 		lastEnd = index + 1
-	}
 
-	// Since we only ever append after we've found a matching
-	// sequence, we still have to append what's left over.
-	if lastEnd > 0 {
+		// Prevent doing the checks we just did again
+		index++
+
+		// We use pretty much the same loop again to keep iterating from this
+		// point tonward without reallocating all the time. This allows us to
+		// stack allocate the buffer, as otherwise, the compiler won't do so.
+		// Comments have been removed, as the logic is equivalent.
+		for ; index < len(input); index++ {
+			if input[index] != ':' {
+				continue
+			}
+
+			if lastEnd > start || index-start == 1 {
+				start = index
+				continue
+			}
+
+			emojiSequence = input[start+1 : index]
+			if lowered := toLower(emojiSequence); lowered != "" {
+				emojiSequence = lowered
+			}
+			emojified = EmojiMap[emojiSequence]
+			if emojified == "" {
+				start = -1
+				index--
+				continue
+			}
+
+			buffer = append(buffer, input[lastEnd:start]...)
+			buffer = append(buffer, emojified...)
+			lastEnd = index + 1
+		}
+
 		buffer = append(buffer, input[lastEnd:]...)
 		return string(buffer)
 	}
